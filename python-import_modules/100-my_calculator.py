911411f41ecbbd0b77c94ebd86f1dcd24126b56e
macharia782 <j.macharia@alustudent.com>
2026-07-21 00:20:29 +0300

    Advanced

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import calculator_1
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    if operator not in ("+", "-", "*", "/"):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    if operator == "+":
+        result = calculator_1.add(a, b)
+    elif operator == "-":
+        result = calculator_1.sub(a, b)
+    elif operator == "*":
+        result = calculator_1.mul(a, b)
+    elif operator == "/":
+        if b == 0:
+            print("Can't divide by zero!")
+            sys.exit(1)
+        result = calculator_1.div(a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
